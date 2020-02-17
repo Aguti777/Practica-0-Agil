15291a4d20c6a5ff2b3ccc2b5e361338fac39e4e
Jose Carlos Mena <jcme0003@red.ujaen.es>
2020-02-17 12:21:46 +0100

    Modificaciones clase 17 febrero

---
+++ undum-master/games/media/games/prision/prision.game.es.js
@@ -0,0 +1,100 @@
+// ---------------------------------------------------------------------------
+// Edit this file to define your game. It should have at least four
+// sets of content: undum.game.situations, undum.game.start,
+// undum.game.qualities, and undum.game.init.
+// ---------------------------------------------------------------------------
+
+/* A unique id for your game. This is never displayed. I use a UUID,
+ * but you can use anything that is guaranteed unique (a URL you own,
+ * or a variation on your email address, for example). */
+undum.game.id = "878da19d-ce25-4b15-820c-728b338a5197"; //Generado por http://www.famkruithof.net/uuid/uuidgen
+
+/* A string indicating what version of the game this is. Versions are
+ * used to control saved-games. If you change the content of a game,
+ * the saved games are unlikely to work. Changing this version number
+ * prevents Undum from trying to load the saved-game and crashing. */
+undum.game.version = "1.0";
+
+/* A variable that changes the fade out speed of the option text on
+ * a mobile. */
+undum.game.mobileHide = 2000
+
+/* A variable that changes the options fade out speed. */
+undum.game.fadeSpeed = 1500
+
+/* A variable that changes the slide up speed after clicking on an
+ * option. */
+undum.game.slideUpSpeed = 500
+
+// En modo depuracion, desactivamos efectos de jquery
+jQuery.fx.off = true;
+
+/* The situations that the game can be in. Each has a unique ID. */
+undum.game.situations = {
+    start: new undum.SimpleSituation(
+        "<h1>Patio de reclusos</h1>\
+        <img width='350' src='media/games/prision/patio-prision.jpg' class='float_right'>\
+        <p>Te encuentras en el patio de reclusos de una prisión de máxima seguridad situada en el desierto de Arizona.</p>\
+        <p>Debido a una fuerte tormenta de arena, la más fuerte desde que se tienen registros de las mismas se te proporciona la oportunidad de poder escapar de la cárcel siempre y cuando tomes las decisiones correctas y no cometas errores en tu intento de fuga.</p>\
+        <p>En este juego eres un prisionero de una prisión de alta seguridad en el desierto de Arizona. Durante el desarrollo del juego se produce una tormenta de magnitudes sin precedentes (nunca se ha registrado una tormenta tan fuerte) que te puede dar la posibilidad de escapar de la cárcel y ser libre.</p>",
+    {
+        enter: function(character, system, from){
+            system.doLink('patio');
+        }
+    }
+    ),
+    patio: new undum.SimpleSituation(
+        "<p>SDFSDFsdfsdfsD</p>"
+    )
+};
+
+// ---------------------------------------------------------------------------
+/* The Id of the starting situation. */
+undum.game.start = "start";
+
+// ---------------------------------------------------------------------------
+/* Here we define all the qualities that our characters could
+ * possess. We don't have to be exhaustive, but if we miss one out then
+ * that quality will never show up in the character bar in the UI. */
+undum.game.qualities = {
+    skill: new undum.IntegerQuality(
+        "Skill", {priority:"0001", group:'stats'}
+    ),
+    stamina: new undum.NumericQuality(
+        "Stamina", {priority:"0002", group:'stats'}
+    ),
+    luck: new undum.FudgeAdjectivesQuality( // Fudge as in the FUDGE RPG
+        "<span title='Skill, Stamina and Luck are reverently borrowed from the Fighting Fantasy series of gamebooks. The words representing Luck are from the FUDGE RPG. This tooltip is illustrating that you can use any HTML in the label for a quality (in this case a span containing a title attribute).'>Luck</span>",
+        {priority:"0003", group:'stats'}
+    ),
+
+    inspiration: new undum.NonZeroIntegerQuality(
+        "Inspiration", {priority:"0001", group:'progress'}
+    ),
+    novice: new undum.OnOffQuality(
+        "Novice", {priority:"0002", group:'progress', onDisplay:"&#10003;"}
+    )
+};
+
+// ---------------------------------------------------------------------------
+/* The qualities are displayed in groups in the character bar. This
+ * determines the groups, their heading (which can be null for no
+ * heading) and ordering. QualityDefinitions without a group appear at
+ * the end. It is an error to have a quality definition belong to a
+ * non-existent group. */
+undum.game.qualityGroups = {
+    stats: new undum.QualityGroup(null, {priority:"0001"}),
+    progress: new undum.QualityGroup('Progress', {priority:"0002"})
+};
+
+// ---------------------------------------------------------------------------
+/* This function gets run before the game begins. It is normally used
+ * to configure the character at the start of play. */
+undum.game.init = function(character, system) {
+    character.qualities.skill = 12;
+    character.qualities.stamina = 12;
+    character.qualities.luck = 0;
+    character.qualities.novice = 1;
+    character.qualities.inspiration = 0;
+    system.setCharacterText("<p>You are starting on an exciting journey.</p>");
+};
