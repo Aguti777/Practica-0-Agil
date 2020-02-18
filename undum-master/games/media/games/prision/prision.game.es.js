3d6b3dc8dd73d99ba0b5d4c7dc860365d7b28285
Jose Carlos Mena <jcme0003@red.ujaen.es>
2020-02-18 17:10:53 +0100

    Primera iteracion historia undum

--- undum-master/games/media/games/prision/prision.game.es.js
+++ undum-master/games/media/games/prision/prision.game.es.js
@@ -33,18 +33,31 @@ jQuery.fx.off = true;
 undum.game.situations = {
     start: new undum.SimpleSituation(
         "<h1>Patio de reclusos</h1>\
+        <p>Te encuentras en el patio de reclusos de una prisión de máxima seguridad situada en el desierto de Arizona.\
         <img width='350' src='media/games/prision/patio-prision.jpg' class='float_right'>\
-        <p>Te encuentras en el patio de reclusos de una prisión de máxima seguridad situada en el desierto de Arizona.</p>\
-        <p>Debido a una fuerte tormenta de arena, la más fuerte desde que se tienen registros de las mismas se te proporciona la oportunidad de poder escapar de la cárcel siempre y cuando tomes las decisiones correctas y no cometas errores en tu intento de fuga.</p>\
-        <p>En este juego eres un prisionero de una prisión de alta seguridad en el desierto de Arizona. Durante el desarrollo del juego se produce una tormenta de magnitudes sin precedentes (nunca se ha registrado una tormenta tan fuerte) que te puede dar la posibilidad de escapar de la cárcel y ser libre.</p>",
-    {
-        enter: function(character, system, from){
-            system.doLink('patio');
-        }
-    }
+        Debido a una fuerte tormenta de arena, la más fuerte desde que se tienen registros de las mismas se te proporciona la oportunidad de poder escapar de la cárcel siempre y cuando tomes las decisiones correctas y no cometas errores en tu intento de fuga.</p>\
+        <p>En este juego eres un prisionero de una prisión de alta seguridad en el desierto de Arizona. Durante el desarrollo del juego se produce una tormenta de magnitudes sin precedentes (nunca se ha registrado una tormenta tan fuerte) que te puede dar la posibilidad de escapar de la cárcel y ser libre.</p>\
+        <hr/>\
+        <p>Durante la fuerte tormenta se produce un apagón eléctrico en la cárcel lo que provoca que la mayoría de los prisioneros aprovechando el desconcierto de los guardas se avalanchen sobre el único guarda que se encuentra en ese momento en el patio.</p>\
+        <p>Se te presenta ahora la primera decisión en esta trepidante aventura, ¿Qué piensas hacer?</p>\
+        <p><a href='ayudarprisionero'>ayudar a los prisioneros</a> o <a href='ayudarguarda'>ayudar al guarda</a> <b>SUERTE EN TUS FUTURAS DECISIONES</b></p>"
     ),
-    patio: new undum.SimpleSituation(
-        "<p>SDFSDFsdfsdfsD</p>"
+    ayudarprisionero: new undum.SimpleSituation(
+        "<p>AYUDAS AL PRISIONERO</p>"
+    ),
+    ayudarguarda: new undum.SimpleSituation(
+        "<p>Has decidido ayudar al guarda y dejar morir al prisionero, por lo que todos los prisioneros se vuelven en tu contra y a partir de ahora deberá evitar encontrarte con ellos, si no quieres acabar mal...</p>\
+        <p>El guarda y tú conseguís huir del lugar y salís corriendo por el pasillo, donde las puertas se encuentran abiertas.</p>\
+        <p>El guarda saca sus esposas y las utilizar para cerrar la puerta y evitar ser alcanzado por los otros guardas. Aprovechando que el guarda está ocupado puedes decidir huir y <a href='huircelda'>salir corriendo hacia tu celda</a>, <a href='huirsalida'>correr a buscar una salida</a> o <a href='continuarconguarda'>continuar con el guarda</a> para intentar llegar al puesto de mando.</p>"
+    ),
+    huircelda: new undum.SimpleSituation(
+        "<p>Has decidido huir hacia tu celda</p>"
+    ),
+    huirsalida: new undum.SimpleSituation(
+        "<p>Has decidido salir corriendo en busca de una salida</p>"
+    ),
+    continuarconguarda: new undum.SimpleSituation(
+        "<p>Has decidido continuar con el guarda</p>"
     )
 };
 
@@ -53,48 +66,29 @@ undum.game.situations = {
 undum.game.start = "start";
 
 // ---------------------------------------------------------------------------
-/* Here we define all the qualities that our characters could
- * possess. We don't have to be exhaustive, but if we miss one out then
- * that quality will never show up in the character bar in the UI. */
-undum.game.qualities = {
-    skill: new undum.IntegerQuality(
-        "Skill", {priority:"0001", group:'stats'}
-    ),
-    stamina: new undum.NumericQuality(
-        "Stamina", {priority:"0002", group:'stats'}
-    ),
-    luck: new undum.FudgeAdjectivesQuality( // Fudge as in the FUDGE RPG
-        "<span title='Skill, Stamina and Luck are reverently borrowed from the Fighting Fantasy series of gamebooks. The words representing Luck are from the FUDGE RPG. This tooltip is illustrating that you can use any HTML in the label for a quality (in this case a span containing a title attribute).'>Luck</span>",
-        {priority:"0003", group:'stats'}
-    ),
-
-    inspiration: new undum.NonZeroIntegerQuality(
-        "Inspiration", {priority:"0001", group:'progress'}
-    ),
-    novice: new undum.OnOffQuality(
-        "Novice", {priority:"0002", group:'progress', onDisplay:"&#10003;"}
-    )
-};
-
-// ---------------------------------------------------------------------------
 /* The qualities are displayed in groups in the character bar. This
  * determines the groups, their heading (which can be null for no
  * heading) and ordering. QualityDefinitions without a group appear at
  * the end. It is an error to have a quality definition belong to a
  * non-existent group. */
 undum.game.qualityGroups = {
-    stats: new undum.QualityGroup(null, {priority:"0001"}),
-    progress: new undum.QualityGroup('Progress', {priority:"0002"})
+    inventario: new undum.QualityGroup('Inventario', {priority:"0001"})
+};
+
+// ---------------------------------------------------------------------------
+/* Here we define all the qualities that our characters could
+ * possess. We don't have to be exhaustive, but if we miss one out then
+ * that quality will never show up in the character bar in the UI. */
+undum.game.qualities = {
+    collar: new undum.IntegerQuality(
+        "Collar", {priority:"0001", group:'inventario', onDisplay:"&#10003;"}
+    )
 };
 
 // ---------------------------------------------------------------------------
 /* This function gets run before the game begins. It is normally used
  * to configure the character at the start of play. */
 undum.game.init = function(character, system) {
-    character.qualities.skill = 12;
-    character.qualities.stamina = 12;
-    character.qualities.luck = 0;
-    character.qualities.novice = 1;
-    character.qualities.inspiration = 0;
-    system.setCharacterText("<p>You are starting on an exciting journey.</p>");
+    system.setQuality("collar", false)
+    system.setCharacterText("<p>¡Comienza tu apasionante aventura!.</p>");
 };
